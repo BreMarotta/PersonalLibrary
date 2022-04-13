f57cde799fe2b5c1d846618dd9c4d0bf578cd600
Breanne <BreMarotta@gmail.com>
2022-04-13 09:34:38 -0400

    set up basics of global state for UserContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./App.css"
 import {  Route, Routes } from "react-router-dom";
+import { UserProvider } from './Components/MyContext'
 import Navigation from './Components/Navigation'
 import Home from './Components/Home'
 import Library from './Components/Library'
@@ -10,14 +11,16 @@ import AddBookForm from './Components/AddBookForm'
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Breanne</h1>
-      <Navigation />
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/library" element={<Library />} />
-        <Route exact path='/new' element={<AddBookForm />} />
-        {/* <Route path='/:id' element={<ShowPage />}/> */}
-      </Routes>
+      <UserProvider>
+        <h1>Hello from Breanne</h1>
+        <Navigation />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/library" element={<Library />} />
+          <Route exact path='/new' element={<AddBookForm />} />
+          {/* <Route path='/:id' element={<ShowPage />}/> */}
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
